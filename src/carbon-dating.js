0e545aab30a33e2e707d18fe81832a60e7fba8f6
enthusiast17 <ulan17nurym@gmail.com>
2020-09-23 23:53:42 +0500

    fix: change return value

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -9,5 +9,5 @@ module.exports = function dateSample(sampleActivity) {
   if (isNaN(N)) return false
   const logN0DivideN = Math.log2(MODERN_ACTIVITY / N) / Math.log2(Math.E)
   const age = Math.ceil((logN0DivideN) / (0.693 / HALF_LIFE_PERIOD))
-  return age > 0 && isFinite(age)
+  return age > 0 && isFinite(age) ? age : false
 };
