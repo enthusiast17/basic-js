cbbb9d2ea0b552f071b3b65559d2f89fbf6979f9
enthusiast17 <ulan17nurym@gmail.com>
2020-09-28 23:50:00 +0500

    feat: add get season

---
+++ kek.js
@@ -0,0 +1,14 @@
+
+const MODERN_ACTIVITY= 15; 
+const HALF_LIFE_PERIOD= 5730;
+
+const solve = (sampleActivity) => {
+    if (typeof sampleActivity !== 'string') return false
+    const N = parseFloat(sampleActivity)
+    if (isNaN(N)) return false
+    const logN0DivideN = Math.log2(MODERN_ACTIVITY / N) / Math.log2(Math.E)
+    const age = Math.ceil((logN0DivideN) / (0.693 / HALF_LIFE_PERIOD))
+    return age > 0 && isFinite(age) ? age : false
+}
+
+console.log(solve('1'))
