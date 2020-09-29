1e9ec8048cf8a1763595c9cc75ecc17aa83486b0
enthusiast17 <ulan17nurym@gmail.com>
2020-09-29 18:46:15 +0500

    feat: add simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  state: [],
+}
+
+chainMaker.addLink = (value) => {
+  chainMaker.state.push(value)
+    return chainMaker
+}
+
+chainMaker.removeLink = (position) => {
+  if (chainMaker.state[position - 1] === undefined) {
+    chainMaker.state = []
+    throw new Error()
   }
-};
+  chainMaker.state.splice(position - 1, 1)
+  return chainMaker
+}
+
+chainMaker.reverseChain = () => {
+  chainMaker.state.reverse()
+  return chainMaker
+}
+
+chainMaker.finishChain = () => {
+  const res = chainMaker.state.slice(1).reduce((res, element) => res + `~~( ${element} )`, `( ${chainMaker.state[0]} )`)
+  chainMaker.state = []
+  return res
+
+}
 
 module.exports = chainMaker;
